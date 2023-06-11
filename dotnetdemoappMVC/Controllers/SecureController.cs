afd29989e5c405817a2c1660a6f3b0a0429c7319
Ido Flatow <ido.flatow@gmail.com>
2023-06-11 10:53:40 +0000

    Add impersonation test

--- dotnetdemoappMVC/Controllers/SecureController.cs
+++ dotnetdemoappMVC/Controllers/SecureController.cs
@@ -65,7 +65,12 @@ namespace DotNetDemoAppMvc.Controllers
                 var trustedDomainWithUserContext = new PrincipalContext(ContextType.Domain, "sub.secondary.lab", "DC=sub,DC=secondary,DC=lab", "sub\\administrator", "P@ssw0rd12!");
                 groupNames.Add(GetPrincipalName(trustedDomainWithUserContext, User.Identity.Name, "Trusted using AD User"));
 
+                var impContext = wi.Impersonate();
                 var trustedDomainContext = new PrincipalContext(ContextType.Domain, "sub.secondary.lab", "DC=sub,DC=secondary,DC=lab");
+                groupNames.Add(GetPrincipalName(trustedDomainContext, User.Identity.Name, "Trusted using impersonation"));
+                impContext.Undo();
+
+                trustedDomainContext = new PrincipalContext(ContextType.Domain, "sub.secondary.lab", "DC=sub,DC=secondary,DC=lab");
                 groupNames.Add(GetPrincipalName(trustedDomainContext, User.Identity.Name, "Trusted using gMSA"));
 
 
